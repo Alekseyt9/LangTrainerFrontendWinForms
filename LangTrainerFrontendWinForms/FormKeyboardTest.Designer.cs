3a1add0143fbaa4abe456a77b7091a4057146242
Alkekseyt <alekseyt9@gmail.com>
2023-04-05 01:08:09 +0300

    -

--- LangTrainerFrontendWinForms/FormKeyboardTest.Designer.cs
+++ LangTrainerFrontendWinForms/FormKeyboardTest.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormKeyboardTest";
+            SuspendLayout();
+            // 
+            // FormKeyboardTest
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(800, 450);
+            Name = "FormKeyboardTest";
+            Text = "FormKeyboardTest";
+            ResumeLayout(false);
         }
 
         #endregion
--- LangTrainerFrontendWinForms/FormKeyboardTest.cs
+++ LangTrainerFrontendWinForms/FormKeyboardTest.cs
@@ -39,7 +39,7 @@ namespace LangTrainerFrontendWinForms
         {
             var newHoveredRectangleIndex = -1;
 
-            var scaleFactor = Math.Min((float)ClientSize.Width / 1100, (float)ClientSize.Height / 500);
+            var scaleFactor = GetScaleFactor();
             for (var i = 0; i < _rectangles.Count; i++)
             {
                 var rectangleInfo = _rectangles[i];
@@ -56,7 +56,7 @@ namespace LangTrainerFrontendWinForms
             if (newHoveredRectangleIndex != hoveredRectangleIndex)
             {
                 hoveredRectangleIndex = newHoveredRectangleIndex;
-                Invalidate(); 
+                Invalidate();
             }
         }
 
@@ -67,13 +67,18 @@ namespace LangTrainerFrontendWinForms
             return (ButtonList)serializer.Deserialize(reader);
         }
 
+        private float GetScaleFactor()
+        {
+            return Math.Min((float)ClientSize.Width / 900, (float)ClientSize.Height / 400);
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
             var graphics = e.Graphics;
             var pen = new Pen(Color.Black);
-            var scaleFactor = Math.Min((float)ClientSize.Width / 1100, (float)ClientSize.Height / 500);
+            var scaleFactor = GetScaleFactor();
             Brush textBrush = new SolidBrush(Color.Black);
             var cornerRadius = (int)(15 * scaleFactor);
 
@@ -81,8 +86,8 @@ namespace LangTrainerFrontendWinForms
             {
                 var rectangleInfo = _rectangles[i];
 
-                var rectangle = new Rectangle((int)(rectangleInfo.X * scaleFactor), 
-                    (int)(rectangleInfo.Y * scaleFactor), (int)(rectangleInfo.W * scaleFactor), (int)(rectangleInfo.H* scaleFactor));
+                var rectangle = new Rectangle((int)(rectangleInfo.X * scaleFactor),
+                    (int)(rectangleInfo.Y * scaleFactor), (int)(rectangleInfo.W * scaleFactor), (int)(rectangleInfo.H * scaleFactor));
 
                 using (var path = new GraphicsPath())
                 {
@@ -101,7 +106,7 @@ namespace LangTrainerFrontendWinForms
                 var font = new Font("Arial", fontSize);
                 var text = rectangleInfo.Text;
                 var textSize = graphics.MeasureString(text, font);
-                
+
                 // Уменьшение размера шрифта, если текст не вмещается в прямоугольник
                 while (textSize.Width > rectangle.Width || textSize.Height > rectangle.Height)
                 {
